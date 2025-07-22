10cbc4c0cf61fdf9eaf2c22fb6759ab15dfb8597
Commonwealth Creative <hello.commonwealthcreative@gmail.com>
2025-07-22 17:41:48 -0400

    Google Tag Manager

--- header.php
+++ header.php
@@ -20,6 +20,13 @@
 			else { echo 'data-wf-page="67b77370aa4177e837b7eaf4"';} ?> 
 			
 			data-wf-site="67b4f78dd1034a4efbd43935">
+			<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5HDR3FSV');</script>
+<!-- End Google Tag Manager -->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -56,6 +63,10 @@ f="XMLHttpRequest",g._w={},g._w[f]=m[f],g._w[s]=m[s],m[s]&&(m[s]=function(){retu
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="ns "
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'allmyhr-mmxxv' ); ?></a>
 
