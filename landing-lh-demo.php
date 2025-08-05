51e2a8f54d34e55d4e779a0a2349f881c62624a6
Commonwealth Creative <hello.commonwealthcreative@gmail.com>
2025-08-05 12:48:05 -0400

    Living Handbook Demo Landing page

---
+++ landing-lh-demo.php
@@ -0,0 +1,236 @@
+<?php
+/**
+ * Template Name:Living Handbook Demo
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package allmyhr-mmxxv
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<section class="hero-section orange">
+    <div id="w-node-b40d9ebe-6b1c-f715-0e38-d4ad2c1fd2f5-5e7a721a" class="w-layout-layout landing-hero wf-layout-layout">
+      <div class="w-layout-cell"><img src="/wp-content/themes/allmyhr-mmxxv/images/allmy_hr_living_handbook_cover.webp" loading="lazy" sizes="(max-width: 1500px) 100vw, 1500px"  alt=""></div>
+      <div class="w-layout-cell center landing">
+        <h1 data-w-id="48c48bed-58bc-bb54-89d7-9fdeb30513f3" style="opacity:0"><span>Always Compliant. Always Current. Always There for You.</span></h1>
+        <p>Your employee handbook is more than a set of policies—it’s the blueprint for your culture, the safeguard for your business, and the foundation for compliance. But keeping it up to date year after year? That’s where most companies fall behind.</p>
+        <a href="https://calendly.com/allmyhr10/allmyhr-product-demo?month=2025-08/?utm_source=allmyhr&utm_medium=landing" class="btn wht w-button" target="_blank">Schedule Your Demo</a>
+        <div class="w-layout-hflex landing-flex-hero">
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>30 Day Money Back Guarantee</strong></h4>
+          </div>
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>No Long Term Contracts </strong></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+	        <div class="crumb"><span class="fa"></span> The Trusted HR Solution by U.S.A Corporations.</div>
+    	<?php get_template_part('template-parts/content', 'trusted'); ?>
+  </section>
+  <section class="content-section bg-white">
+    <div class="container">
+      <h2 class="match1 center">A Handbook That Evolves With Your Business.</h2>
+      <p class="center">A handbook isn’t just a list of policies—it’s the foundation of your compliance, your culture, and your communication with employees. But most businesses create it once and let it collect dust, leaving them exposed to costly compliance gaps. The truth is, keeping a handbook compliant year‑round is nearly impossible without expert guidance. That’s why you need ongoing, informed oversight from specialists who understand both the law—and how it applies to your unique workplace.</p>
+      <h3 class="center"><span class="highlight txt">Keep Your Handbook Compliant, Your Culture Strong, and Your Business Protected—All Year Long.</span></h3><img src="/wp-content/themes/allmyhr-mmxxv/images/Screenshot-2025-03-04-at-10.26.35-AM-p-1080.png" loading="lazy" alt="" class="center img">
+      <h3 class="center big">With your Living Handbook Subscription, you’ll receive:</h3>
+      <div id="w-node-_393bb891-1442-0b52-7baa-f3661dd7c51e-5e7a721a" class="w-layout-layout _900-width wf-layout-layout">
+        <div class="w-layout-cell">
+          <div class="w-layout-hflex phrases">
+            <div class="fa highlight blu txt"></div>
+            <h4><strong>Custom Federal & State Compliant Handbook Build</strong> — Create your complete, compliant handbook in under an hour.</h4>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div class="w-layout-hflex phrases">
+            <div class="fa highlight blu txt"></div>
+            <h4><strong>Real-Time Compliance Monitoring</strong> — We track Federal and State changes for you.</h4>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div class="w-layout-hflex phrases">
+            <div class="fa highlight blu txt"></div>
+            <h4><strong>One-Click Updates</strong>— If a change applies to your company, simply click “Incorporate” to instantly update your handbook.</h4>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div class="w-layout-hflex phrases">
+            <div class="fa highlight blu txt"></div>
+            <h4><strong>Multi-Language & Multi-State Options</strong> — Available in English, Spanish, and tailored for multi-state businesses.</h4>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div class="w-layout-hflex phrases">
+            <div class="fa highlight blu txt"></div>
+            <h4><strong>Ongoing Expert Guidance</strong> — Access our HR compliance specialists for answers and direction. </h4>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div class="w-layout-hflex phrases">
+            <div class="fa highlight blu txt"></div>
+            <h4><strong>Automatic Policy Version History</strong> — Every update is saved with a timestamp, so you can track changes and reference past versions anytime.</h4>
+          </div>
+        </div>
+      </div>
+      <div class="center">
+        <a href="https://calendly.com/allmyhr10/allmyhr-product-demo?month=2025-08/?utm_source=allmyhr&utm_medium=landing" class="btn w-button" target="_blank">Schedule Your Demo</a>
+        <div class="w-layout-hflex landing-flex-hero">
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>30 Day Money Back Guarantee</strong></h4>
+          </div>
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>No Long Term Contracts </strong></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content-section bg-dkblue"></section>
+  <section class="content-section bg-dkblue bg-gradientblack">
+    <div class="container h-content center overflownone">
+      <div data-delay="3000" data-animation="slide" class="slider-wrapper testimonials w-slider" data-autoplay="true" data-easing="ease" data-hide-arrows="false" data-disable-swipe="false" data-w-id="6e6de18a-3c21-a882-3f6a-c51d5c6bbe49" data-autoplay-limit="0" data-nav-spacing="3" data-duration="500" data-infinite="true">
+        <h2 class="center">What Our Clients Say</h2>
+        <div class="fa crumb highlight blu txt"></div>
+        <div class="slider-wrapper-mask w-slider-mask">
+          <div class="slide-item w-slide">
+            <div class="slider-card blu">
+              <div class="slide-testimonial">
+                <div class="w-layout-hflex phrases">
+                  <div class="fa highlight blu jumbo txt"></div>
+                  <div>AllMyHR is an amazing program. We recently updated some company policies in our employee handbook with the “Living Handbook” program which made it easy and painless. What would’ve taken us a week to make sure we dotted all the I’s and crossed all the T’s only took us 2 days. It’s made a tedious part of my job enjoyable.</div>
+                </div>
+                <h3 class="center">Shamika P.<br><span class="crumb highlight txt">General Manager</span></h3>
+              </div>
+            </div>
+          </div>
+          <div class="slide-item w-slide">
+            <div class="slider-card blu">
+              <div class="slide-testimonial">
+                <div class="w-layout-hflex phrases">
+                  <div class="fa highlight blu jumbo txt"></div>
+                  <div>AllMyHR is a goldmine for any Human Resources practitioner! The system has everything right at the click of a mouse. I use the training and certification platform to keep the leadership team informed and prepared. Additionally, the compliance calendar, employee handbook creator, and advisors are second to none.</div>
+                </div>
+                <h3 class="center">Delores W.<br><span class="crumb highlight txt">General Manager</span></h3>
+              </div>
+            </div>
+          </div>
+          <div class="slide-item w-slide">
+            <div class="slider-card blu">
+              <div class="slide-testimonial">
+                <div class="w-layout-hflex phrases">
+                  <div class="fa highlight blu jumbo txt"></div>
+                  <div>We have found the AllMyHR platform to be extremely easy to use. It’s a good feeling to know I can pick up the phone or send an email and get a quick, reliable response to my question. I highly recommend the AllMyHR platform of services.</div>
+                </div>
+                <h3 class="center">Barbara W.<br><span class="crumb highlight txt">Content Creator</span></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider-arrow-left small w-slider-arrow-left">
+          <div class="fa highlight txt"></div>
+        </div>
+        <div class="slider-arrow-right small w-slider-arrow-right">
+          <div class="fa highlight txt"></div>
+        </div>
+        <div class="slider-indicators w-slider-nav w-round"></div>
+      </div>
+    </div>
+  </section>
+  <section class="content-section bg-white">
+    <div class="w-layout-blockcontainer container w-container">
+      <h2 class="center match1">Expert Compliance &amp; On-Demand Updates</h2>
+      <p class="center">Our Living Handbook Builder enables you to create your company’s Federal and State compliant handbook in less than an hour. Once it’s built, you can usually keep it compliant in less than an hour each year. Strengthen your workplace without adding to your payroll—and stay focused on what matters most: running your business.</p>
+      <h3 class="center big">Never Miss a Change. Never Risk a Penalty.</h3>
+      <div id="w-node-_68eac608-53fe-6e0d-6a88-e3ee61ac9380-5e7a721a" class="w-layout-layout wf-layout-layout">
+        <div class="w-layout-cell">
+          <div data-w-id="68eac608-53fe-6e0d-6a88-e3ee61ac9382" style="opacity:0" class="card">
+            <div class="fa highlight txt match1"></div>
+            <h3 class="highlight txt"><strong>Save Time & Money.</strong></h3>
+            <div class="crumb">Build your handbook once and keep it current in minutes, not days.</div>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div data-w-id="68eac608-53fe-6e0d-6a88-e3ee61ac9389" style="opacity:0" class="card">
+            <div class="fa highlight txt match1"></div>
+            <h3 class="highlight txt"><strong>Stay Protected.</strong></h3>
+            <div class="crumb">Avoid the massive liability of an out-of-date handbook when you need it most.</div>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div data-w-id="68eac608-53fe-6e0d-6a88-e3ee61ac9390" style="opacity:0" class="card">
+            <div class="fa highlight txt match1"></div>
+            <h3 class="highlight txt"><strong>Peace of Mind.</strong></h3>
+            <div class="crumb">We proactively alert you when laws change—so you’ll never miss a critical update.</div>
+          </div>
+        </div>
+        <div class="w-layout-cell">
+          <div data-w-id="68eac608-53fe-6e0d-6a88-e3ee61ac9397" style="opacity:0" class="card">
+            <div class="fa highlight txt match1"></div>
+            <h3 class="highlight txt"><strong>Higher ROI Tasks.</strong></h3>
+            <div class="crumb">Let us handle the compliance legwork while you focus on growing your business.</div>
+          </div>
+        </div>
+      </div>
+      <div class="center">
+        <a href="https://calendly.com/allmyhr10/allmyhr-product-demo?month=2025-08/?utm_source=allmyhr&utm_medium=landing" class="btn w-button" target="_blank">Schedule Your Demo</a>
+        <div class="w-layout-hflex landing-flex-hero">
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>30 Day Money Back Guarantee</strong></h4>
+          </div>
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>No Long Term Contracts </strong></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content-section orange">
+    <div class="w-layout-blockcontainer container w-container">
+      <h1 data-w-id="b2ed670e-c9e3-470e-0d68-672560be930f" style="opacity:0" class="center"><span>Your Handbook—Built Once, Protected Forever.</span></h1>
+      <p class="center">With the Living Handbook, compliance isn’t a one‑time event—it’s a year‑round safeguard for your business. We keep your policies current, your risks low, and your culture clear so you can focus on leading with confidence.</p>
+      <div id="w-node-_05642b30-419f-d859-d033-ef8fc9e98aac-5e7a721a" class="w-layout-layout landing-hero bottom wf-layout-layout">
+        <div class="w-layout-cell center">
+          <h3>Stay Confident, Stay Compliant</h3>
+          <div>&quot;A handbook that’s out of date is a liability. The Living Handbook keeps you compliant and confident—no matter how often the rules change&quot;</div>
+          <h3 class="center">Ben Rudasill<br><span class="crumb">AllMyHR Co-Founder</span></h3>
+        </div>
+        <div style="padding-top: 20px;" class="w-layout-cell"><img src="/wp-content/themes/allmyhr-mmxxv/images/allmy_hr_living_handbook_cover.webp" loading="lazy" alt=""></div>
+        <div class="w-layout-cell center">
+          <h3>Protect Your Business Without the Guesswork</h3>
+          <div>“We built the Living Handbook to remove the guesswork from compliance, so every business can protect itself and its people without losing focus on growth.”</div>
+          <h3 class="center">Stuart Jackson<br><span class="crumb">AllMyHR Co-Founder</span></h3>
+        </div>
+      </div>
+      <div class="center">
+        <a href="https://calendly.com/allmyhr10/allmyhr-product-demo?month=2025-08/?utm_source=allmyhr&utm_medium=landing" class="btn wht w-button" target="_blank">Schedule Your Demo</a>
+        <div class="w-layout-hflex landing-flex-hero">
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>30 Day Money Back Guarantee</strong></h4>
+          </div>
+          <div class="w-layout-hflex phrases">
+            <div class="fa _20-10-margin"></div>
+            <h4><strong>No Long Term Contracts </strong></h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+	</main><!-- #main -->
+
+<?php
+get_footer();
